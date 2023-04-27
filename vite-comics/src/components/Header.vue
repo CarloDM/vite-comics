7a092a8f05ec4a2d01bf16391520f8cf5d7f8ec9
carloDM <carlo.de.mauro.work@gmail.com>
2023-04-27 21:52:28 +0200

    header layout

--- vite-comics/src/components/Header.vue
+++ vite-comics/src/components/Header.vue
@@ -4,6 +4,19 @@ export default {
   components: {},
   data( ){
     return {
+      logoImg : '../assets/vue-dc-comics-1/img/dc-logo.png',
+      links : [
+        {name: 'characters',    link:'#', active: false},
+        {name: 'comics',        link:'#', active: false},
+        {name: 'moves',         link:'#', active: false},
+        {name: 'tv',            link:'#', active: false},
+        {name: 'games',         link:'#', active: false},
+        {name: 'collectibles',  link:'#', active: false},
+        {name: 'videos',        link:'#', active: false},
+        {name: 'fans',          link:'#', active: false},
+        {name: 'news',          link:'#', active: false},
+        {name: 'shop',          link:'#', active: false},
+      ]
       
     }
   }
@@ -12,9 +25,12 @@ export default {
 
 <template>
   <div class="header debug">
-    <div class="logo debug"></div>
+    <div class="logo debug">
+      <img src="../assets/vue-dc-comics-1/img/dc-logo.png" alt="">
+    </div>
 
     <div class="options debug">
+      <span v-for="(link, index) in links">{{ link.name }}</span>
 
     </div>
 
@@ -25,7 +41,28 @@ export default {
 
 <style scoped>
 .header{
+  display: flex;
+  justify-content: space-between;
   width: 100%;
   height: 80px;
+}
+
+.logo{
+
+  width: 80px;
+  height: 80px;
+}
+
+.logo img {
+  width: 100%;
+}
+.options{
+  display: flex;
+  align-items: center;
+}
+
+.options span{
+  display: inline-block;
+  margin:5px
 }
 </style>
